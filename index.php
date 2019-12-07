25bd437b09aa2cf6ccdf77466bc11d17046fa79e
mohamadsyalvasr <33284353+mohamadsyalvasr@users.noreply.github.com>
2019-12-07 21:24:21 +0700

    https

--- index.php
+++ index.php
@@ -141,7 +141,7 @@ elseif(isset($_GET["Cleanup"]))
 <html>
 <head>
     <title>Submission Mohamad Syalva Syalabi Rosyidy</title>
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
 </head>
 <body>
  
